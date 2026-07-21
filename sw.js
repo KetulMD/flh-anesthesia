5c864346842c73174a166ca718df91a340e389de
KetulMD <ketulmd@gmail.com>
2026-07-21 11:44:24 -0400

    Expand antibody reference (table + significance/crossmatch/HDN); SW flh-v15

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    - CDN libraries (jsdelivr): cache-first after first use.
    Bump CACHE_VERSION whenever files change to push updates to installed apps. */
 
-const CACHE_VERSION = 'flh-v14';
+const CACHE_VERSION = 'flh-v15';
 const PRECACHE = [
   './',
   './index.html',
