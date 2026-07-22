075171a3c0a6981a5f25eb34db1e8d4ef87aefc4
KetulMD <ketulmd@gmail.com>
2026-07-22 19:14:19 -0400

    Dark mode for bmi/drug-labels/or-emergencies/neuromonitoring/sab-notes (sw v28)

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    - CDN libraries (jsdelivr): cache-first after first use.
    Bump CACHE_VERSION whenever files change to push updates to installed apps. */
 
-const CACHE_VERSION = 'flh-v27';
+const CACHE_VERSION = 'flh-v28';
 const PRECACHE = [
   './',
   './index.html',
