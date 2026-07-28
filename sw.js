be6aceabb9a49e7fe8d6a8146e2ea75472d7591c
KetulMD <ketulmd@gmail.com>
2026-07-28 14:50:21 -0400

    Batch 12: 6 Peri-Op entries (liver transplant, Down syndrome, hemophilia/vWD, acromegaly, bowel obstruction, GBS); sw v43

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    - CDN libraries (jsdelivr): cache-first after first use.
    Bump CACHE_VERSION whenever files change to push updates to installed apps. */
 
-const CACHE_VERSION = 'flh-v42';
+const CACHE_VERSION = 'flh-v43';
 const PRECACHE = [
   './',
   './index.html',
